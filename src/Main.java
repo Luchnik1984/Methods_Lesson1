c8d4f3996af0ed417d25297caa7f85539daec4f7
Luchnik1984 <luchnik1984@mail.ru>
2025-03-27 23:27:09 +0300

    1 task is ready commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    public static String isLeapYear(int year) {
+        if (year < 1584) {
+            return year + " год. Вы ввели год, старше 1584 года, первого високосного года ";
+        } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return year + " - Это високосный год.";
+        } else {
+            return year + " - Это не високосный год.";
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Задача 1.
+        System.out.println("\nЗадача 1.");
+        System.out.print("Определитель високосного года\nВведите год не старше 1584: ");
+        Scanner input = new Scanner(System.in);
+        int year;
+        year = input.nextInt();
+        System.out.println(isLeapYear(year));
     }
 }
